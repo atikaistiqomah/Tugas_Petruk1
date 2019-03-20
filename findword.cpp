9d667d08a3720bc5c7567d32e285bdc685aacf26
Atika Istiqomah-1817051004 <48738106+atika1004@users.noreply.github.com>
2019-03-20 22:10:02 +0700

    lanjutan looping untuk mencari kata

--- findword.cpp
+++ findword.cpp
@@ -4,8 +4,30 @@ using namespace std;
 
 
 
-for (int k=0;k<lenght_word;k++) {
+for (int k=0;k<length_word;k++) {
   if (input[k]==table[z-k][z]) {
+    periksa=k;
+  } else {break;}
+}
+
+  if (periksa==length_word-1) {
+    word_finded+=1;
+  } else {word_finded+=0;}
+periksa=0;
+
+for (int k=0;k<length_word;k++) {
+  if (input[k]==table[z-k][p-k]) {
+    periksa=k;
+  } else {break;}
+}
+
+  if (periksa==length_word-1) {
+  word_finded+=1;
+} else {word_finded+=0;}
+periksa=0;
+
+for (int k=0;k<length_word;k++) {
+  if (input[k]==table[z-k][p+k]) {
     periksa=k;
   } else {break;}
 }
